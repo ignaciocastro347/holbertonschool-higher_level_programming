68b4e89d622a9f2d148dc6b791d6165d22d04c16
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-09 16:56:00 -0300

    Write a program that prints the number of and the list of its arguments.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     import sys
     argv = sys.argv
-    print("{} argument{}".format(
+    print("{} argument{}{}".format(
         len(argv) - 1,
         "s" if len(argv) - 1 != 1 else "",
         ":" if len(argv) - 1 != 0 else "."))
