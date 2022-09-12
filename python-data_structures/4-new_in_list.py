053adf8f0c5e4838b68b63211309bc8dedf65c93
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-12 19:43:56 -0300

    Write a function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def new_in_list(my_list, idx, element):
     copy_list = list(my_list)
-    if my_list:
+    if (my_list and idx in range(0, len(my_list))):
         copy_list[idx] = element
     return copy_list
