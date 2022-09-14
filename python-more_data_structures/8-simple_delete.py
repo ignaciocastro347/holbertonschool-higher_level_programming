cc32a71549da0526c6866c27f76d78df85d7ac1e
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-13 22:00:26 -0300

    Write a function that deletes a key in a dictionary.

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-	if key != "" :
-		a_dictionary.pop(key, None)
-	return a_dictionary
+    if key != "" :
+        a_dictionary.pop(key, None)
+    return a_dictionary
