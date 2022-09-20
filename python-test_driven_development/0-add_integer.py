486e0396f756a2c962e6867feedac99c4bc60f6c
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-20 16:44:00 -0300

    Write a function that adds 2 integers.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 0-add-integer module """
+
+
+def add_integer(a, b=98):
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
