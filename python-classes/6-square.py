25cf6a65af6a067fbabc33de20d45d534cfcef9f
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-20 12:50:12 -0300

    Write a class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,6 +40,6 @@ class Square:
         if self.__size == 0:
             print()
             return
-        print("\n" * (self.position[1] - 1))        
+        print("\n" * (self.__position[1] - 1))        
         for i in range(self.__size):
-            print(" " * self.position[0] + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
