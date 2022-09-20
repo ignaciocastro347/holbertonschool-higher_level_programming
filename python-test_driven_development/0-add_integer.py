336e01f6542a9235ce171f3a9c619c84614ba5f6
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-20 17:42:51 -0300

    Write a function that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    if type(a) != int and type(a) != float:
+    if a is None or type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
