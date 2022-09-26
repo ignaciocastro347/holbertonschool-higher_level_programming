412d6ff0629bc24bc2696a14f30e875c719058ea
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-26 16:32:10 -0300

    Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False.

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Recrtangle module """
+
+
+def is_same_class(obj, a_class):
+    """ function to check if object is instance of a class """
+    return True if type(obj) == a_class else False
