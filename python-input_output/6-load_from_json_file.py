c6d39bbd7aa4ad895aa71b739eecbef082f6b5e9
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 15:05:22 -0300

    6. Create object from a JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ load_from_json_file function """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
