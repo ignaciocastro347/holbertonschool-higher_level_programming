c219ef2e93e1b74ee916be5c1c864674714ed133
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 16:11:16 -0300

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Student module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dict = self.__dict__
+        if (type(attrs) == list and all(type(x) == str for x in attrs)):
+            return {key: dict[key] for key in set(attrs) & set(dict.keys())}
+        return dict
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
