ba9ffce835d7aa2fa8c9e0a6d3340f10251c1bca
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 12:55:27 -0300

    7. Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" BaseGeometry module """
+
+
+from typing import Type
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{name} must be an integer")
+        if value <= 0:
+            raise ValueError("{name} must be greater than 0")
