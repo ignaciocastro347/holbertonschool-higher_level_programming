cd469b2ea31fcf047205a6075c399840665ac7a9
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 15:47:25 -0300

    Write a class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Student module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list
+            and type(x) == str for x in attrs):
+            return { key: self.__dict__[key] for key in attrs }
+        return self.__dict__
