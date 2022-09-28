e086f8de5562b652ee5f8333bc63bcc40b1c9f0c
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 14:56:45 -0300

    5. Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ save_to_json_file function """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(json.dump(my_obj))
+        json.dump(my_obj, f)
