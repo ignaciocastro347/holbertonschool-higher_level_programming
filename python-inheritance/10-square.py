cfb8b6d0e272474908ca6a937da80d1c99603b6a
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 14:03:49 -0300

    10. Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Square class """
     def __init__(self, size):
-        self.integer_validation("size", size)
+        self.integer_validator("size", size)
         self._size = size
 
     def area(self):
