420c68c2fc644765fed1abef9a812141ce3dfedc
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 15:18:38 -0300

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
     load_file = __import__('6-load_from_json_file').load_from_json_file
     argv = sys.argv
 
-    list = load_from_json_file("add_item.json")
+    try:
+        list = load_file("add_item.json")
+    except Exception:
+        list = []
     list += argv[1:]
-    save_to_json_file(list, "add_item.json")
+    save_to_file(list, "add_item.json")
