260012f765e59f663d59065c3f458d61d14915d2
root <root@DESKTOP-AJ89OOU.localdomain>
2022-09-28 17:41:43 -0300

    12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" pascal_triangle module """
+
+
+def pascal_triangle(n):
+    """ pascal_triangle function"""
+    if n <= 0: return []
+
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            if j == 0:
+                val = 1
+            else:
+                try:
+                    val = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                except IndexError:
+                    val = triangle[i - 1][j - 1]
+            triangle[i].append(val)
+    return triangle
