eff8cea353b8c23884d6c807fd3945c1987e1dba
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-01 20:58:58 -0300

    Write the first class Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         if id:
-           self.id = id
+            self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
