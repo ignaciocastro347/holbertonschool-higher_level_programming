209a3da1978772162cd861200fefcd69f7bdb406
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-03 22:21:55 -0300

    3. Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Rectangle model """
+from typing import Type
 from .base import Base
 
 
@@ -7,10 +8,10 @@ class Rectangle(Base):
     """ Rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -18,6 +19,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value <= 0):
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -26,6 +31,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        if (value <= 0):
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -34,6 +43,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if (type(value) != int):
+            raise TypeError("x must be an integer")
+        if (value < 0):
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -42,4 +55,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if (type(value) != int):
+            raise TypeError("y must be an integer")
+        if (value < 0):
+            raise ValueError("y must be >= 0")
         self.__y = value
