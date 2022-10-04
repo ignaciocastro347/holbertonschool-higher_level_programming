bffe1fc53436805b4be76492c1841fd52c18bb81
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-03 22:36:31 -0300

    4. Area first & 5. Display #0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -60,3 +60,10 @@ class Rectangle(Base):
         if (value < 0):
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def display(self):
+        for row in range(self.__height):
+            print("#" * self.__width)
