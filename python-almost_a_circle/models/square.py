62a5d91981b98a72d64c21a4c96ec3f248c967a5
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-05 20:58:14 -0300

    10. And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,3 +12,16 @@ class Square(Rectangle):
         """ return a resume of this Square instance """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("y must be an integer")
+        if (value < 0):
+            raise ValueError("y must be >= 0")
+        self.__width = value
+        self.__height = value
