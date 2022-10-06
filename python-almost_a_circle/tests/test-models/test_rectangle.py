b021efea5c9b083934811086cb0c3469d9bcbbb1
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 19:39:22 -0300

    0. If it's not tested it doesn't work

---
+++ python-almost_a_circle/tests/test-models/test_rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Unittest for Rectangle class """
+import unittest
+Rectangle = __import__('../../models/Rectangle.py').Rectangle
+
+class TestRectangle(unittest.TestCase):
+    pass
---
+++ python-almost_a_circle/tests/test-models/test_square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Unittest for Square class """
+import unittest
+Square = __import__('../../models/square.py').Square
+
+class TestSquare(unittest.TestCase):
+    pass
