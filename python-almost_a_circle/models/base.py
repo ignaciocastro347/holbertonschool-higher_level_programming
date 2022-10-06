9501359520c172b50739eba10b8ff3c992534722
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-05 22:08:33 -0300

    16. JSON string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,3 +20,12 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save_to_json_file function """
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                json.dump(list_objs, f)
