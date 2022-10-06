1e9fa1a4bb2ebdc23a1a0ed681ed6164fa94f091
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-05 21:37:53 -0300

    15. Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 	
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ return the JSON string repr of list_dictionaries """
         return "[]" if list_dictionaries is None else json.dumps(list_dictionaries)
