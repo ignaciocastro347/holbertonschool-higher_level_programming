497dc832bf43529ecf00aa9b942b6a89581e370d
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 22:38:54 -0300

    0. If it's not tested it doesn't work

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -10,13 +10,20 @@ class TestRectangle(unittest.TestCase):
         self.r3 = Rectangle(1, 2, 3, 4)
 
     def test_init1(self):
-        self.assertEqual(self.r1.to_dictionary(), {"id": 1, "width": 1, "height": 2, "x": 0, "y":0})
+        self.assertEqual(self.r1.width, 1)
+        self.assertEqual(self.r1.height, 2)
 
     def test_init2(self):
-        self.assertEqual(self.r1.to_dictionary(), {"id": 1, "width": 1, "height": 2, "x": 0, "y":0})
+        self.assertEqual(self.r2.width, 1)
+        self.assertEqual(self.r2.height, 2)
+        self.assertEqual(self.r2.x, 3)
 
     def test_init3(self):
-        self.assertEqual(self.r1.to_dictionary(), {"id": 1, "width": 1, "height": 2, "x": 0, "y":0})
+        self.assertEqual(self.r3.width, 1)
+        self.assertEqual(self.r3.height, 2)
+        self.assertEqual(self.r3.x, 3)
+        self.assertEqual(self.r3.y, 4)
+
 
 class TestRectangle(unittest.TestCase):
     def test_init(self):
