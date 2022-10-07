47404dd7eb32ec7be78192ca304d56e402f9e7a3
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 23:11:08 -0300

    0. If it's not tested it doesn't work

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -32,7 +32,7 @@ class TestRectangle(unittest.TestCase):
 
 
 
-class TestRectangle(unittest.TestCase):
+class TestRectangleExceptions(unittest.TestCase):
     def test_init(self):
         self.assertRaises(TypeError, Rectangle, "1", 2)
         self.assertRaises(TypeError, Rectangle, 1, "2")
@@ -41,9 +41,30 @@ class TestRectangle(unittest.TestCase):
 
     def test_init2(self):
         self.assertRaises(ValueError, Rectangle, -1, 2)
-        self.assertRaises(ValueError, Rectangle, -1, -2)
+        self.assertRaises(ValueError, Rectangle, 1, -2)
         self.assertRaises(ValueError, Rectangle, 0, 2)
         self.assertRaises(ValueError, Rectangle, 1, 0)
         self.assertRaises(ValueError, Rectangle, 1, 2, -3)
         self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
+
+class TestRectangleArea(unittest.TestCase):
+    def test_area(self):
+        r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r.area(), 2)
+
+class TestRectangleStr(unittest.TestCase):
+    def test_str(self):
+        r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r.__str__(), "[Rectangle] (1) 3/4 - 1/2")
+
+class TestRectangleDisplay(unittest.TestCase):
+    def test_display(self):
+        r1 = Rectangle(1, 2)
+        r2 = Rectangle(1, 2, 1)
+        r3 = Rectangle(1, 2, 1, 1)
+        self.assertEquals(r1.display(), "#\n#")
+        self.assertEquals(r2.display(), " #\n #")
+        self.assertEquals(r3.display(), "\n #\n #")
+
+
 
