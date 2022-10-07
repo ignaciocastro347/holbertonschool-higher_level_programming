899a9e3542b4aa6472433d761ab7f3fb03c43349
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 22:45:58 -0300

    0. If it's not tested it doesn't work

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -4,21 +4,19 @@ import unittest
 from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
-    def setUp(self):
-        self.r1 = Rectangle(1, 2)
-        self.r2 = Rectangle(1, 2, 3)
-        self.r3 = Rectangle(1, 2, 3, 4)
-
     def test_init1(self):
+        self.r1 = Rectangle(1, 2)
         self.assertEqual(self.r1.width, 1)
         self.assertEqual(self.r1.height, 2)
 
     def test_init2(self):
+        self.r2 = Rectangle(1, 2, 3)
         self.assertEqual(self.r2.width, 1)
         self.assertEqual(self.r2.height, 2)
         self.assertEqual(self.r2.x, 3)
 
     def test_init3(self):
+        self.r3 = Rectangle(1, 2, 3, 4)
         self.assertEqual(self.r3.width, 1)
         self.assertEqual(self.r3.height, 2)
         self.assertEqual(self.r3.x, 3)
