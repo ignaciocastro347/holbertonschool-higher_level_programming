f8a64e6b0b6695a3aa5302304519d3d3bcf63f0d
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 21:31:09 -0300

    0. If it's not tested it doesn't work

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -3,5 +3,12 @@
 import unittest
 from models.rectangle import Rectangle
 
-class TestRectangle(unittest.TestCase):
-    pass
+class TestFromJsonString(unittest.TestCase):
+    def setUp(self):
+        self.r1 = Rectangle(1, 2)
+        self.r2 = Rectangle(1, 2, 3)
+        self.r3 = Rectangle(1, 2, 3, 4)
+        # self.r4 = Rectangle("1", 2)
+
+    def test_init(self):
+        self.assertEqual(self.r1.id, 1)
