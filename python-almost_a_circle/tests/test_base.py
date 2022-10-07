d5396568e7927fbc415d8a981d332e93a7335856
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-06 21:13:43 -0300

    0. If it's not tested it doesn't work

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -15,3 +15,17 @@ class TestBase(unittest.TestCase):
     
     def test_init3(self):
         self.assertEqual(self.b.id, 3)
+
+class TestRepeatedBase(unittest.TestCase):
+    def setUp(self):
+        self.b = Base(1)
+
+    def test_init(self):
+        self.assertEqual(self.b.id, 1)
+
+class TestToJsonString(unittest.TestCase):
+    def setUp(self):
+        self.b = Base()
+
+    def test_to_json_string(self):
+        self.assertEqual(self.b.to_json_string(None), "[]")
