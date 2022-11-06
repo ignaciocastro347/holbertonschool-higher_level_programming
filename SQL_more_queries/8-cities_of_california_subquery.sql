d8489ac4a8d4aa35cbe9bcdf745080af913901a2
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-06 19:59:19 -0300

    8. Cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- list all the cities of California that can be found in the database hbtn_0d_usa.
+--   - The states table contains only one record where
+-- 	   name = California (but the id can be different, as per the example)
+--   - Results must be sorted in ascending order by cities.id
+--   - You are not allowed to use the JOIN keyword
+SELECT c.* FROM cities as c, states as s 
+WHERE s.id = c.state_id AND s.name = 'California' 
+ORDER BY c.id ASC;
