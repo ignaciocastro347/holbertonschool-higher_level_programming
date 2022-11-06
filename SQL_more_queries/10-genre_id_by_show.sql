431eb192cb9f00c0cfa7a31cadcc6f0cd96f5d95
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-06 20:32:40 -0300

    10. Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 --   - Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --   - You can use only one SELECT statement
 USE hbtn_0d_tvshows;
-SELECT s.title, g.id AS genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id;
+SELECT s.title, pivot.genre_id
+FROM tv_shows s
+JOIN tv_show_genres pivot ON s.id = pivot.show_id
+ORDER BY s.title, pivot.genre_id;
