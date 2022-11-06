dac514355080c87b318d5aa676c37690e3401819
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-06 20:08:52 -0300

    9. Cities by States

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- list all cities contained in the database hbtn_0d_usa.
+--   - Each record should display: cities.id - cities.name - states.name
+--   - Results must be sorted in ascending order by cities.id
+--   - You can use only one SELECT statement
+SELECT c.id, c.name, s.name FROM cities c 
+JOIN states s
+ORDER BY c.id ASC;
