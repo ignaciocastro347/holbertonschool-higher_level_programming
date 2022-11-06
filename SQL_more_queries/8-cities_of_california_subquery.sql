105c1a8df385fcf748ddeb0a67aa580bf79b43bd
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-06 20:00:21 -0300

    8. Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,6 @@
 -- 	   name = California (but the id can be different, as per the example)
 --   - Results must be sorted in ascending order by cities.id
 --   - You are not allowed to use the JOIN keyword
-SELECT c.* FROM cities as c, states as s 
+SELECT c.* FROM cities AS c, states AS s 
 WHERE s.id = c.state_id AND s.name = 'California' 
 ORDER BY c.id ASC;
