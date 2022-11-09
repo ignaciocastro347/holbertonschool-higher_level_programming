565f67983a67e4d7cff3e9fc285d8ee08f61169d
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-08 23:20:27 -0300

    1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" lists all states """
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'n%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
