72ffc38dc216b27279a4f4b222e9edc9c4c28f9b
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-10 22:51:41 -0300

    6. First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" State model """
-
-
+"""
+    State model
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
