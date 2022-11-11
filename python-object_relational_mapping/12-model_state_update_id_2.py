a71f8ff421ac2ca9ad93cbbdda22d7a021e4d791
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-11 00:22:07 -0300

    12. Update a state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,18 +3,18 @@
 """
 
 if __name__ == "__main__":
-	import sys
-	from model_state import Base, State
-	from sqlalchemy.orm import sessionmaker
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
 
-	from sqlalchemy import (create_engine)
+    from sqlalchemy import (create_engine)
 
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-		sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-	Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-	Session = sessionmaker(bind=engine)
-	session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-	session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
-	session.commit()
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
+    session.commit()
