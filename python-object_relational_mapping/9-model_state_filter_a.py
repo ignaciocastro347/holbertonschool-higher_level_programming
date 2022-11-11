85b287a97557cef2d1e971467a179911ea73b7ad
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-10 23:17:41 -0300

    9. Contains

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" list all State objects from the database
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    from sqlalchemy import (create_engine)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
