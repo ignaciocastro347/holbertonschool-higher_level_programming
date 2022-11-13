e86d7ccb1ac7be2a6471dbbd86510dad7334a649
Ignacio Castro <ignaciocastro347@gmail.com>
2022-11-13 16:59:39 -0300

    14. Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -14,4 +14,5 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state.id'))
+    state_id = Column('state_id', String(
+        128), ForeignKey('states.id'), nullable=False)
