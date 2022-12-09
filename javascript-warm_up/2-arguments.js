73be6b5fddf9e03c94261966d4135fc0472f7e14
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-09 20:48:35 -0300

    2. Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const amountArgs = process.argv.length() - 2;
+
+if (amountArgs === 0) {
+  console.log('No argument');
+} else if (amountArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
