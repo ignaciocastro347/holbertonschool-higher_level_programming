2e366990e9d20df71659f48ab949e0365cf1c953
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-09 22:11:18 -0300

    6. Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const lang in langs) {
-  console.log(lang);
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }
