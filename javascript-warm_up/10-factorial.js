7f009dd3aebebd18b362e5c02d1ffb5417cdef9c
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-09 23:40:18 -0300

    10. Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,9 +4,12 @@ const args = Object.values(process.argv).slice(2);
 
 const n = Number(args[0]);
 
-let factorial = 1;
-for (let i = n; i > 1; i--) {
-  factorial *= i;
+function factorial (n) {
+  let factorial = 1;
+  for (let i = n; i > 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
-console.log(factorial);
+console.log(factorial(n));
