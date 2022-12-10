c17011d3fba8c3293911fcebae1c1ccc169f0520
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-10 00:22:37 -0300

    11. Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const args = Object.values(process.argv).slice(2);
 
-if (!args[0] || Number(args[0]) === 1) {
+if (!args[0] || (Number(args[0]) === 1 && !args[1])) {
   console.log(0);
 } else {
   const sorted = args.slice().sort((a, b) => a - b);
