ad3a43d062803a2aed41c51b12a3e06a16ad463b
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-09 21:35:41 -0300

    3. Value of my argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,7 +4,5 @@ const args = Object.values(process.argv).slice(2);
 if (!args[0]) {
   console.log('No argument');
 } else {
-  for (arg of args) {
-    console.log(arg);
-  }
+  console.log(arg[0]);
 }
