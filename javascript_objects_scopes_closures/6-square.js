a4b600fd27110e8f0c38a2b2a77653101ebea26d
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-10 01:36:23 -0300

    6. Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square2 = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Square2 {
   constructor (size) {
     super(size, size);
   }
