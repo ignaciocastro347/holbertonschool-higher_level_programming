67f6851d5879a398f30b2d56d9e7a08d600883c9
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-09 21:58:19 -0300

    5. An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = Object.values(process.argv).slice(2);
-
-if (!Number.isInteger(args[0])) {
+const number = parseInt(args[0])
+if (Number.isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(args[0]));
+  console.log(number);
 }
