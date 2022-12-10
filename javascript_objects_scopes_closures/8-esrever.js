a1516d0a0b86d5af3d1aa282f367c133f3fbecb3
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-10 01:54:31 -0300

    8. Esrever

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  new_list = [];
-  list.forEach(item => new_list.unshift(item));
-  return new_list;
+  const newList = [];
+  list.forEach(item => newList.unshift(item));
+  return newList;
 };
