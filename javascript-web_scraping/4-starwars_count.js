c8d422d5c993cb7f5b7ea154a2098743b38d9a66
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-15 21:19:50 -0300

    4. Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
+const args = Object.values(process.argv).slice(2);
 
-request('https://swapi-api.hbtn.io/api/films', function (error, response, body) {
+request(args[0], function (error, response, body) {
   if (error) console.log(error);
   const data = JSON.parse(body);
   const movies = data.results;
