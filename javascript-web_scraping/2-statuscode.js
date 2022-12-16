c42aa53cd037000b83dc7f1b8b477443afb62e35
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-15 21:01:14 -0300

    2. Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const args = Object.values(process.argv).slice(2);
 
 request(args[0], function (error, response, body) {
   if (error) console.log(error);
-  console.log('code: ', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
 });
